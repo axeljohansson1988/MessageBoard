e4612a3625dfc25530cbd48b634ac5cbffee2c7f
Axel Johansson <axeljohansson88@outlook.com>
2020-11-30 16:57:58 +0100

    added apiModels (interfaces).

---
+++ messageboardapp/src/models/apiModels.tsx
@@ -0,0 +1,33 @@
+interface IBaseModel {
+    id?: number;
+    created?: Date;
+    modified?: Date;
+};
+
+interface IBaseResponse {
+    status: string;
+    operationSuccess: boolean;
+}
+
+export interface IBoardMessage extends IBaseModel {
+    message: string;
+    clientId?: number;
+    isMine: boolean;
+};
+
+export interface IClient extends IBaseModel {
+    name: string;
+};
+
+export interface IBoardMessageReponse extends IBaseResponse {
+    boardMessage: IBoardMessage;
+};
+
+export interface IClientReponse extends IBaseResponse {
+    client: IClient;
+};
+
+
+
+
+
